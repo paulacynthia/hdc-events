d638900633099aa180d1369d9d62e8018a48dc2a
paulacynthia <pcynthia013@gmail.com>
2021-08-04 16:30:50 -0300

    Flash Message

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -32,6 +32,6 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -44,7 +44,17 @@
       </div>
     </nav>
   </header>
-  @yield('content')
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        @if(session('msg'))
+        <p class="msg">{{session('msg')}}
+        </p>
+        @endif
+        @yield('content')
+      </div>
+    </div>
+  </main>
 
   <footer>
     <p>&copy; HDC Events - 2021</p>
