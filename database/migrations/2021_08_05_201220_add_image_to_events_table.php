4a9164b52d2740cd5f00977fa9a4324305f8d12e
paulacynthia <pcynthia013@gmail.com>
2021-08-05 17:33:19 -0300

    Image Upload

--- database/migrations/2021_08_05_201220_add_image_to_events_table.php
+++ database/migrations/2021_08_05_201220_add_image_to_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class AddImageToEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 100);
-            $table->integer('qty');
-            $table->text('description');
-            $table->timestamps();
+        Schema::table('events', function (Blueprint $table) {
+            $table->string('image');
         });
     }
 
@@ -29,6 +25,8 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::table('events', function (Blueprint $table) {
+            $table->dropColumn('image');
+        });
     }
 }
